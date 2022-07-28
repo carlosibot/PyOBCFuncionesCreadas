291fb8a9d7f3dcb29facf88be362fa18f66d314c
carlosibot <106968244+carlosibot@users.noreply.github.com>
2022-07-28 19:47:44 -0400

    Agregando Proyecto

---
+++ FuncionesCreadas.py
@@ -0,0 +1,7 @@
+import Operaciones as op
+
+print(op.operaciones.suma(8,5))
+print(op.operaciones.resta(8,5))
+print(op.operaciones.producto(8,5))
+print(op.operaciones.division(8,5))
+
---
+++ Operaciones.py
@@ -0,0 +1,9 @@
+class operaciones:
+    def suma(a,b):
+        return a + b
+    def resta(a,b):
+        return a - b
+    def producto(a,b):
+        return a * b
+    def division(a,b):
+        return a / b
